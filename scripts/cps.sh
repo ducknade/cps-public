2a482a65fb353c87e3a1f14d4c728f3fddd9a68d
HummingTree <ducknade@gmail.com>
2017-03-08 16:03:54 -0500

    simplify cps.sh

--- scripts/cps.sh
+++ scripts/cps.sh
@@ -23,63 +23,21 @@ profile () {
     time { "$@" >>$wd/log 2>&1 ; }
 }
 
-if [ $arch = bgq ] ; then
+cd "$cps"
 
-    profile rsync -avz --delete $wd/cps_pp/ ./cps_pp/
-    cd cps_pp
-    profile autoconf
-    cd "$cps"
-    mkdir build
-    cd build
-    profile ../cps_pp/configure \
-    	--build=powerpc64-unknown-linux-gnu --host=powerpc64-bgq-linux \
-	--enable-target=bgq \
-        --enable-debug=no \
-        --enable-fftw=$prefix \
-        --enable-qmp=$prefix \
-        --enable-xml=$prefix \
-        --enable-qio \
-        --enable-gmp \
-	--enable-bfm=$prefix/bfm \
-        --enable-gsl=$prefix \
-	--enable-eigen=$prefix
-	profile make -j60
-	# profile make
-    cd "$cps"
-
-elif [ $arch = amd64 ] ; then
-
-##    profile rsync -avz --delete $wd/cps_pp/ ./cps_pp/
-##    cd cps_pp
-##    profile autoconf
-##    cd "$cps"
-##    mkdir build
-##    cd build
-#    profile ../cps_pp/configure \
-#        --enable-fftw=$prefix \
-#        --enable-eigen=$prefix \
-#        --enable-debug=no
-##    profile ../cps_pp/configure --enable-debug=no
-#    profile make -j30
-##	profile make
-
-	cd "$cps"
-
-    profile rsync -avz --delete $wd/cps_pp/ ./cps_pp-qmp/
-    cd cps_pp-qmp
-    profile autoconf
-    cd "$cps"
-    mkdir build-qmp
-    cd build-qmp
-    profile ../cps_pp-qmp/configure \
-        --enable-qmp=$prefix \
-        --enable-qio \
-        --enable-sse=no \
-        --enable-debug=no
-	profile make -j30
-    cd "$cps"
-
-fi
+profile rsync -avz --delete $wd/cps_pp/ ./cps_pp-qmp/
+cd cps_pp-qmp
+profile autoconf
+cd "$cps"
+mkdir build-qmp
+cd build-qmp
+profile ../cps_pp-qmp/configure \
+    --enable-qmp=$prefix \
+    --enable-qio \
+    --enable-sse=no \
+    --enable-debug=no
+profile make -j30
+cd "$cps"
 
 echo
 cd $wd
